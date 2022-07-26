c29dcf9b812dbe6d6a9f2f59dadfb1df2b3eec12
dannypellatt <danielpellatt92@gmail.com>
2022-07-26 11:18:12 -0400

    completed exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,24 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Racecar", true)] //Add test data <-------
+        [InlineData("notapalindrome", false)]
+        [InlineData("emordnilappalindrome", true)]
+        //[InlineData(0, -3, -2, -5)]
+        public void Test1(string word, bool expected)
         {
+            //arrange
+            var palindrome = new WordSmith();
+            //act
+            var actual = palindrome.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
 
         }
     }
